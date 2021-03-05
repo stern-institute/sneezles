6ada57c6c2e452f4ae9de590db88d370e5a08836
Sam Brown <sneezles@sambrown.eu>
2021-03-05 23:24:50 +0000

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm run build
+
+gsutil -m cp -r dist/* gs://www.sneezl.es
+gsutil web set -m index.html gs://www.sneezl.es
+gsutil setmeta -h "Content-Type:text/html" 'gs://www.sneezl.es/*.html'
