f8980a80ffd96b0254db9b68b926994af39747d8
Sam Brown <sneezles@sambrown.eu>
2021-03-19 11:36:24 +0000

    Add WebsiteCarbon link

--- src/App.vue
+++ src/App.vue
@@ -36,16 +36,31 @@
     <v-footer>
       <v-spacer />
       &copy;2021 - Quentin Saint
-      <a href="https://twitter.com/SneezlesProject"
-        ><svg-icon color="#1da1f2" type="mdi" :path="mdiTwitter"
-      /></a>
-      <a href="mailto:hotmail@sneezl.es"><svg-icon type="mdi" :path="mdiEmail" /></a>
-      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        ><svg-icon color="#c4302b" type="mdi" :path="mdiYoutube"
-      /></a>
-      <a href="https://github.com/stern-institute/sneezles"
-      ><svg-icon color="black" type="mdi" :path="mdiGithub"
-      /></a>
+      <a href="https://twitter.com/SneezlesProject">
+        <svg-icon color="#1da1f2" type="mdi" :path="mdiTwitter" />
+      </a>
+      <a href="mailto:hotmail@sneezl.es">
+        <svg-icon type="mdi" :path="mdiEmail" />
+      </a>
+      <v-tooltip top>
+        <template v-slot:activator="{ on, attrs }">
+          <a
+            v-bind="attrs"
+            v-on="on"
+            href="https://www.websitecarbon.com/website/sneezl-es/"
+          >
+            <svg-icon color="black" type="mdi" :path="mdiMoleculeCo2" />
+          </a>
+        </template>
+        WebsiteCarbon says: Only 0.43 g of CO<sub>2</sub> is produced every time
+        someone visits this web page.
+      </v-tooltip>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+        <svg-icon color="#c4302b" type="mdi" :path="mdiYoutube" />
+      </a>
+      <a href="https://github.com/stern-institute/sneezles">
+        <svg-icon color="black" type="mdi" :path="mdiGithub" />
+      </a>
       <v-spacer />
     </v-footer>
   </v-app>
@@ -57,7 +72,13 @@ import SneezleMap from "@/components/SneezleMap.vue";
 import FAQs from "@/components/FAQs.vue";
 import About from "@/components/About.vue";
 import SvgIcon from "@jamescoyle/vue-icon";
-import { mdiTwitter, mdiEmail, mdiYoutube, mdiGithub } from "@mdi/js";
+import {
+  mdiEmail,
+  mdiGithub,
+  mdiMoleculeCo2,
+  mdiTwitter,
+  mdiYoutube,
+} from "@mdi/js";
 
 export default Vue.extend({
   name: "App",
@@ -70,6 +91,7 @@ export default Vue.extend({
   data: () => ({
     mdiEmail,
     mdiGithub,
+    mdiMoleculeCo2,
     mdiTwitter,
     mdiYoutube,
   }),
