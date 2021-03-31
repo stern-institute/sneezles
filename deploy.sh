f24518482f7aee873bd2c5dd8acddbcabe0b9315
Sam Brown <sneezles@sambrown.eu>
2021-03-31 20:48:13 +0100

    Set cache-control (5 mins)

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ npm run build
 
 gsutil -m cp -Z -r dist/* gs://www.sneezl.es
 gsutil web set -m index.html gs://www.sneezl.es
-gsutil setmeta -h "Content-Type:text/html" 'gs://www.sneezl.es/*.html'
+gsutil setmeta -h "Content-Type:text/html" -h "Cache-Control: public, max-age=300" 'gs://www.sneezl.es/*.html'
