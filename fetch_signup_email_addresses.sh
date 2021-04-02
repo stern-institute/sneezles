cb5282b8303cd79e89e22e3f972ee6084e4edb09
Sam Brown <sneezles@sambrown.eu>
2021-04-02 12:33:41 +0100

    Include BigQuery schema in repo

--- fetch_signup_email_addresses.sh
+++ fetch_signup_email_addresses.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bq load --skip_leading_rows=1 sneezles_log_analysis.usage 'gs://sneezles-logging/UsageLogs_usage*' ~/Downloads/cloud_storage_usage_schema_v0.json
+bq load --skip_leading_rows=1 sneezles_log_analysis.usage 'gs://sneezles-logging/UsageLogs_usage*' cloud_storage_usage_schema_v0.json
 bq query "select cs_object as email_address from sneezles_log_analysis.usage where cs_uri contains 'signup' group by email_address"
